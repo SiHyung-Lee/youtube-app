32826e1ce6a4669ed4bbb6d387fa39264076294a
isihyeong <buzzcatfish@gmail.com>
2020-02-28 00:48:31 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import api from './Components/Api';
-import VideoList from './Components/VideoList';
-import VideoDetail from './Components/VideoDetail';
+import React from "react";
+import api from "./Components/Api";
+import VideoList from "./Components/VideoList";
+import VideoDetail from "./Components/VideoDetail";
 
 class App extends React.Component {
-    state = {
-        videos: [],
-        video: ''
-    };
+  state = {
+    videos: [],
+    video: ""
+  };
 
-    handlevideo = async searchTerm => {
-        const result = await api.get('/search', {
-            params: {
-                q: searchTerm
-            }
-        });
+  handlevideo = async searchTerm => {
+    const result = await api.get("/search", {
+      params: {
+        q: searchTerm
+      }
+    });
 
-        this.setState({
-            videos: result.data.items,
-            video: result.data.items[0]
-        });
-    };
+    this.setState({
+      videos: result.data.items,
+      video: result.data.items[0]
+    });
+  };
 
-    componentDidMount() {
-        this.handlevideo('iphone');
-    }
+  componentDidMount() {
+    this.handlevideo("iphone");
+  }
 
-    render() {
-        const { videos, video } = this.state;
+  render() {
+    const { videos, video } = this.state;
 
-        return (
-            <div>
-                <VideoDetail video={video} />
-                <VideoList videos={videos} />
-            </div>
-        );
-    }
+    return (
+      <div>
+        <VideoDetail video={video} />
+        <VideoList videos={videos} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/VideoList.js
+++ src/Components/VideoList.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
 class VideoList extends React.Component {
-    render() {
-        const { videos } = this.props;
-        return (
-            <div>
-                {videos.map((item, id) => (
-                    <div key={id}>
-                        <img
-                            src={item.snippet.thumbnails.default.url}
-                            alt={item.snippet.title}
-                        />
-                        <p>{item.snippet.title}</p>
-                    </div>
-                ))}
-            </div>
-        );
-    }
+  render() {
+    const { videos } = this.props;
+    return (
+      <div>
+        {videos.map((item, id) => (
+          <div key={id}>
+            <img
+              src={item.snippet.thumbnails.default.url}
+              alt={item.snippet.title}
+            />
+            <p>{item.snippet.title}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default VideoList;
